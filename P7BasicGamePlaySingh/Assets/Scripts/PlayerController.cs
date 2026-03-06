b53620588a7f77e64bcd1105287a15fb2821e113
768650-cell <768650@student.dusd.net>
2026-03-06 15:24:53 -0800

    Unit 2 Basic game play.
    I am almost done I am on spawn random animal.

--- P7BasicGamePlaySingh/Assets/Scripts/PlayerController.cs
+++ P7BasicGamePlaySingh/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 16;
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +17,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange )
+            transform.position = new Vector3( xRange , transform.position.y, transform.position.z);
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
