1026dba5a9f64ec5a53cdcecaf9e67845002f06e
768650-cell <768650@student.dusd.net>
2026-03-12 15:26:54 -0700

    Unit 2 Basic   game play
    I done my project and i am on bonus features.

--- P7BasicGamePlaySingh/Assets/Scripts/PlayerController.cs
+++ P7BasicGamePlaySingh/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 16;
+    public float zRange = 10;
     public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,15 +23,21 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         if (transform.position.x > xRange )
             transform.position = new Vector3( xRange , transform.position.y, transform.position.z);
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (transform.position.z < -zRange)
+            transform.position = new Vector3(-zRange, transform.position.y, transform.position.x);
+        if (transform.position.z > zRange)
+            transform.position = new Vector3(zRange, transform.position.y, transform.position.x);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
-        
-        
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
     }
 }
